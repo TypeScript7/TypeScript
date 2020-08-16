87c2a88cc4cc8d326c49aeb0a5aeac4452ce0d13
Americanman <snowboardracer7@gmail.com>
2020-08-16 00:34:24 -0400

    Type variables before we give them a value

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,29 @@
-var character = 'mario';
-var age = 20;
-var isBlackBelt = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// explicit types, type(verb) variables before we gibe them a value
+var character;
+var age;
+var isLogedIn;
+age = 30;
+isLogedIn = true;
+// arrays 
+var ninjas = [];
+ninjas.push('Vladimir');
+// union types
+var mixed = [];
+mixed.push('hello');
+mixed.push(20);
+var uid;
+uid = '123';
+uid = 123;
+// objects
+var ninjaOne;
+ninjaOne = {
+    name: 'Vladimir',
+    age: 33,
+    beltColor: 'black'
 };
-console.log(circ(7.5));
+var ninjaTwo;
+ninjaTwo = {
+    name: 'Biker',
+    age: 26,
+    beltColor: 'black'
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,46 @@
-let character = 'mario';
-let age = 20;
-let isBlackBelt = true;
+// explicit types, type(verb) variables before we gibe them a value
+let character;
+let age: number;
+let isLogedIn: boolean;
 
-const circ = (diameter: number) => {
-	return diameter * Math.PI
-} 
+age = 30;
 
-console.log(circ(7.5));
+isLogedIn = true;
+
+// arrays 
+
+let ninjas: string[] = [];
+
+ninjas.push('Vladimir');
+
+// union types
+let mixed: (string|number|boolean)[] = [];
+
+mixed.push('hello');
+mixed.push(20)
+
+let uid: string|number;
+uid = '123';
+uid = 123;
+
+// objects
+
+let ninjaOne: object;
+
+	 ninjaOne = {
+		 name: 'Vladimir',
+		 age: 33,
+		 beltColor: 'black'
+}
+
+let ninjaTwo: {
+	name: string,
+	age: number,
+	beltColor: string
+}
+
+ninjaTwo = {
+	name: 'Biker',
+	age: 26,
+	beltColor: 'black'
+}
